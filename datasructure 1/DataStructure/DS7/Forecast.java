c64c50938a73203276088f061f75a33f2be8bd66
Sharan Shaagithiya <sakthishaahith0220@gmail.com>
2025-07-28 10:43:30 +0530

    Delete datasructure 1/DataStructure directory

--- datasructure 1/DataStructure/DS7/Forecast.java
+++
@@ -1,6 +0,0 @@
-public class Forecast {
-    public double futureValue(double currentValue, double rate, int years) {
-        if (years == 0) return currentValue;
-        return futureValue(currentValue * (1 + rate), rate, years - 1);
-    }
-}
--- datasructure 1/DataStructure/DS7/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Forecast f = new Forecast();
-        double result = f.futureValue(1000, 0.1, 5);
-        System.out.println("Future Value: " + result);
-    }
-}

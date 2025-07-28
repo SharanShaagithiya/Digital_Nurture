420ad2f7b47b6ec82528f378aa1f5fffb9245be0
Sharan Shaagithiya <sakthishaahith0220@gmail.com>
2025-07-28 10:43:05 +0530

    Delete DesignPattern directory

--- DesignPattern/exercise4/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        PaymentProcessor stripe = new StripeAdapter();
-        stripe.processPayment(100.0);
-
-        PaymentProcessor paypal = new PayPalAdapter();
-        paypal.processPayment(200.0);
-    }
-}
--- DesignPattern/exercise4/StripeGateway.java
+++
@@ -1,5 +0,0 @@
-public class StripeGateway {
-    public void makeStripePayment(double amount) {
-        System.out.println("Processed $" + amount + " using Stripe.");
-    }
-}
